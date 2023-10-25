4830f7420e27e7c484ff4aaddd1d3016b561bac2
SimonWithoutGarfunkle <123770440+SimonWithoutGarfunkle@users.noreply.github.com>
2023-10-25 22:11:38 +0200

    add patients list html and controller

---
+++ src/main/java/com/medilabo/front/FrontApplication.java
@@ -0,0 +1,20 @@
+package com.medilabo.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class FrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
+}
--- src/test/java/com/medilabo/front/FrontApplicationTests.java
+++ src/test/java/com/medilabo/front/FrontApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class P9FrontMicroserviceApplicationTests {
+class FrontApplicationTests {
 
 	@Test
 	void contextLoads() {
